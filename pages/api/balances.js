5719e7c09ce170cbca8df60b8d77193221b9d894
KingzRex <toochukwukingz6@gmail.com>
2022-11-13 07:46:21 +0000

    set-up API endpoint to check balances

---
+++ pages/api/balances.js
@@ -0,0 +1,27 @@
+import Moralis from 'moralis';
+import { EvmChain } from '@moralisweb3/evm-utils';
+
+export default async function handler(req, res) {
+    // reads the api key from .env.local and starts Moralis SDK
+    await Moralis.start({ apiKey: process.env.MORALIS_API_KEY });
+
+    const address = '0x675951c3ECe300707A075d2f2714783284897E0A';
+
+    const [nativeBalance, tokenBalances] = await Promise.all([
+        Moralis.EvmApi.balance.getNativeBalance({
+            chain: EvmChain.BSC,
+            address,
+
+        }),
+        Moralis.EvmApi.token.getWalletTokenBalances({
+            chain: EvmChain.BSC,
+            address,
+        }),
+    ]);
+
+    res.status(200).json({
+        // formatting the output
+        nativeBalance: nativeBalance.result.balance.ether,
+        tokenBalances: tokenBalances.result.map((token) => token.display()),
+    });
+}
